92dbfff2f48b8d84fb984134fedfc5ff2da2f86d
ARULMURUGAN.N <natrajarul2016@gmail.com>
2024-07-02 12:47:05 +0530

    Update Calculator.jsx

--- problem1/src/Pages/Calculator.jsx
+++ problem1/src/Pages/Calculator.jsx
@@ -1,73 +1,55 @@
+import React, { useState } from "react";
 import axios from "axios";
-import { useState } from "react";
 
 const Calculator = () => {
-  //const [windowPrevState, setWindowPrevState] = useState([]);
   const [windowCurrState, setWindowCurrState] = useState([]);
   const [numbers, setNumbers] = useState([]);
-  console.log(numbers)
   const [avg, setAvg] = useState(0);
   let tempAvg = 0;
+
   const handleInput = async (valtype) => {
-    if (valtype === "prime") {
-      try {
-        const res = await axios.get("http://20.244.56.144/test/primes", {
-          headers: {
-            "Authorization":"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiZXhwIjoxNzE1MTYwNDgyLCJpYXQiOjE3MTUxNjAxODIsImlzcyI6IkFmZm9yZG1lZCIsImp0aSI6ImU0NGQ3MzBmLTVhMDUtNGRjNS05Zjc5LTg4YjUxMzExODkxNiIsInN1YiI6InNlYmUyazA0QGdtYWlsLmNvbSJ9LCJjb21wYW55TmFtZSI6ImdlbnJpbyIsImNsaWVudElEIjoiZTQ0ZDczMGYtNWEwNS00ZGM1LTlmNzktODhiNTEzMTE4OTE2IiwiY2xpZW50U2VjcmV0IjoiTkpMYWt0WlF4eUdhQVFQcSIsIm93bmVyTmFtZSI6InNlYmVzYXJhdmFuYW4iLCJvd25lckVtYWlsIjoic2ViZTJrMDRAZ21haWwuY29tIiwicm9sbE5vIjoiNzMyMTIxMTA0MDY4In0.WsWQAORAmWbKYULBS9dPWD7Ly8FDlvi3Ws-3WBS_Bzs"
-          },
-        });
-        setNumbers(res.data);
-        setWindowCurrState(res.data);
-      } catch (error) {
-        console.log(error);
+    try {
+      let endpoint = "";
+      switch (valtype) {
+        case "prime":
+          endpoint = "http://20.244.56.144/test/primes";
+          break;
+        case "fibonacci":
+          endpoint = "http://20.244.56.144/test/fibo";
+          break;
+        case "even":
+          endpoint = "http://20.244.56.144/test/even";
+          break;
+        case "random":
+          endpoint = "http://20.244.56.144/test/rand";
+          break;
+        default:
+          break;
       }
-    } else if (valtype === "fibonacci") {
-      try {
-        const res = await axios.get("http://20.244.56.144/test/fibo", {
-            headers: {
-                "Authorization":"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiZXhwIjoxNzE1MTYwNDgyLCJpYXQiOjE3MTUxNjAxODIsImlzcyI6IkFmZm9yZG1lZCIsImp0aSI6ImU0NGQ3MzBmLTVhMDUtNGRjNS05Zjc5LTg4YjUxMzExODkxNiIsInN1YiI6InNlYmUyazA0QGdtYWlsLmNvbSJ9LCJjb21wYW55TmFtZSI6ImdlbnJpbyIsImNsaWVudElEIjoiZTQ0ZDczMGYtNWEwNS00ZGM1LTlmNzktODhiNTEzMTE4OTE2IiwiY2xpZW50U2VjcmV0IjoiTkpMYWt0WlF4eUdhQVFQcSIsIm93bmVyTmFtZSI6InNlYmVzYXJhdmFuYW4iLCJvd25lckVtYWlsIjoic2ViZTJrMDRAZ21haWwuY29tIiwicm9sbE5vIjoiNzMyMTIxMTA0MDY4In0.WsWQAORAmWbKYULBS9dPWD7Ly8FDlvi3Ws-3WBS_Bzs"
-              },
-        });
-        setNumbers(res.data);
-        setWindowCurrState(res.data);
-      } catch (error) {
-        console.log(error);
-      }
-    } else if (valtype === "even") {
-      try {
-        const res = await axios.get("http://20.244.56.144/test/even", {
-            headers: {
-                "Authorization":"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiZXhwIjoxNzE1MTYwNDgyLCJpYXQiOjE3MTUxNjAxODIsImlzcyI6IkFmZm9yZG1lZCIsImp0aSI6ImU0NGQ3MzBmLTVhMDUtNGRjNS05Zjc5LTg4YjUxMzExODkxNiIsInN1YiI6InNlYmUyazA0QGdtYWlsLmNvbSJ9LCJjb21wYW55TmFtZSI6ImdlbnJpbyIsImNsaWVudElEIjoiZTQ0ZDczMGYtNWEwNS00ZGM1LTlmNzktODhiNTEzMTE4OTE2IiwiY2xpZW50U2VjcmV0IjoiTkpMYWt0WlF4eUdhQVFQcSIsIm93bmVyTmFtZSI6InNlYmVzYXJhdmFuYW4iLCJvd25lckVtYWlsIjoic2ViZTJrMDRAZ21haWwuY29tIiwicm9sbE5vIjoiNzMyMTIxMTA0MDY4In0.WsWQAORAmWbKYULBS9dPWD7Ly8FDlvi3Ws-3WBS_Bzs"
-              },
-        });
-        setNumbers(res.data);
-        setWindowCurrState(res.data);
-      } catch (error) {
-        console.log(error);
-      }
-    } else {
-      try {
-        const res = await axios.get("http://20.244.56.144/test/rand", {
-            headers: {
-                "Authorization":"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiZXhwIjoxNzE1MTYwNDgyLCJpYXQiOjE3MTUxNjAxODIsImlzcyI6IkFmZm9yZG1lZCIsImp0aSI6ImU0NGQ3MzBmLTVhMDUtNGRjNS05Zjc5LTg4YjUxMzExODkxNiIsInN1YiI6InNlYmUyazA0QGdtYWlsLmNvbSJ9LCJjb21wYW55TmFtZSI6ImdlbnJpbyIsImNsaWVudElEIjoiZTQ0ZDczMGYtNWEwNS00ZGM1LTlmNzktODhiNTEzMTE4OTE2IiwiY2xpZW50U2VjcmV0IjoiTkpMYWt0WlF4eUdhQVFQcSIsIm93bmVyTmFtZSI6InNlYmVzYXJhdmFuYW4iLCJvd25lckVtYWlsIjoic2ViZTJrMDRAZ21haWwuY29tIiwicm9sbE5vIjoiNzMyMTIxMTA0MDY4In0.WsWQAORAmWbKYULBS9dPWD7Ly8FDlvi3Ws-3WBS_Bzs"
-              },
-        });
-        setNumbers(res.data);
-        setWindowCurrState(res.data);
-      } catch (error) {
-        console.log(error);
-      }
+
+      const res = await axios.get(endpoint, {
+        headers: {
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiZXhwIjoxNzE5OTAwNTMzLCJpYXQiOjE3MTk5MDAyMzMsImlzcyI6IkFmZm9yZG1lZCIsImp0aSI6IjI3NTIyY2IxLTViNWItNGJmOS1hN2I2LWFjZjViYzVkNTVlZSIsInN1YiI6ImFydWxtdXJ1Z2FuNTIyMDEyQGdtYWlsLmNvbSJ9LCJjb21wYW55TmFtZSI6Ik5BTSBDb21wYW55IiwiY2xpZW50SUQiOiIyNzUyMmNiMS01YjViLTRiZjktYTdiNi1hY2Y1YmM1ZDU1ZWUiLCJjbGllbnRTZWNyZXQiOiJOdWhQVEJlU3NlVWxjYWtkIiwib3duZXJOYW1lIjoiQXJ1bE11cnVnYW4iLCJvd25lckVtYWlsIjoiYXJ1bG11cnVnYW41MjIwMTJAZ21haWwuY29tIiwicm9sbE5vIjoiMTAifQ.jYHrnTylNiwVexEZd6VMARy7ODPG41sVm92axZqn8Hc",
+        },
+      });
+
+      setNumbers(res.data);
+      setWindowCurrState(res.data);
+    } catch (error) {
+      console.log("Error fetching data:", error);
     }
   };
 
   const handleCalculate = () => {
-    for (var i = 0; i < numbers.length; i++) {
-      tempAvg = tempAvg + numbers[i];
-      if (i == numbers.legnth - 1) {
-        setAvg(tempAvg / numbers.length);
-      }
+    for (let i = 0; i < numbers.length; i++) {
+      tempAvg += numbers[i];
+    }
+    if (numbers.length > 0) {
+      setAvg(tempAvg / numbers.length);
     }
   };
+
   return (
     <div>
       <div className="">
